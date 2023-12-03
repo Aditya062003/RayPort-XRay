9d9889bfb969470f3d754af6f4b1f90a78d94b54
Aditya Vinay Dubey <adityavinay@iitbhilai.ac.in>
2023-12-03 11:23:35 +0530

    Added frontend

---
+++ final evaluation/frontend/src/App.js
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [file, setFile] = useState(null);
+  const [prediction, setPrediction] = useState(null);
+  const [textInput, setTextInput] = useState('');
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+
+    // Create a preview URL for the selected image
+    const previewURL = URL.createObjectURL(selectedFile);
+    setImagePreview(previewURL);
+  };
+
+  const handleSubmit = () => {
+    if (file) {
+      // Image prediction
+      const formData = new FormData();
+      formData.append('file', file);
+
+      axios.post('https://stunning-memory-p5q9xwr677gf6rj9-5000.app.github.dev/predict', formData)
+        .then(response => {
+          setPrediction(response.data.prediction);
+        })
+        .catch(error => console.error('Error:', error));
+    } else if (textInput.trim() !== '') {
+      // Chat submission
+      console.log('User typed:', textInput);
+      // Clear the text input field after submission
+      setTextInput('');
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col relative">
+      {/* Navbar */}
+      <nav className="bg-blue-500 p-4 text-white">
+        <h1 className="text-2xl font-bold">RayPort</h1>
+      </nav>
+
+      <div className='flex mx-8 my-4 justify-center'>
+        <div className='w-1/4'>
+        {imagePreview && (
+          <div className="flex-shrink-0 mx-auto">
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="mb-4 w-60 h-60 rounded-md shadow"
+              style={{ maxWidth: '100%', maxHeight: '600px' }}  // Increase the max height
+            />
+          </div>
+        )}
+        </div>
+        <div className='w-3/4'>
+        {prediction && (
+            <div className="mb-8">
+              <h2 className="text-xl font-bold mb-2">Prediction:</h2>
+              <div className="">
+                <table className="table-auto">
+                  <thead>
+                    <tr>
+                      <th className="px-4 py-2">Condition</th>
+                      <th className="px-4 py-2">Probability</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {prediction.map((prob, index) => (
+                      <tr key={index}>
+                        <td className="border px-4 py-2">{conditionLabels[index]}</td>
+                        <td className="border px-4 py-2">{prob}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
+
+      <div className="fixed bottom-0 left-0 right-0 p-4 bg-white flex">
+        {/* Image Upload Section */}
+        <div className="mb-2">
+          <input
+            type="file"
+            id="file"
+            className="mt-1 p-2 border rounded-md"
+            onChange={handleFileChange}
+            accept="image/*"
+          />
+        </div>
+
+        {/* Chat Input Section */}
+        <div className="ml-4 mx-4 px-4 w-full">
+          <input
+            type="text"
+            value={textInput}
+            onChange={(e) => setTextInput(e.target.value)}
+            placeholder="Type something..."
+            className="border mx-2 mt-1 px-4 py-2 w-full"
+          />
+        </div>
+
+        {/* Submit Button */}
+        <button className="bg-blue-500 mx-4 text-white px-4 py-2 rounded" onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
+
+const conditionLabels = [
+  'No Finding',
+  'Enlarged Cardiomediastinum',
+  'Cardiomegaly',
+  'Lung Opacity',
+  'Lung Lesion',
+  'Edema',
+  'Consolidation',
+  'Pneumonia',
+  'Atelectasis',
+  'Pneumothorax',
+  'Pleural Effusion',
+  'Pleural Other',
+  'Fracture',
+  'Support Devices'
+];
+
+export default App;
