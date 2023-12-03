18a9377ae8bf74583f360fadf3ca6e11b1c9faf6
tusharb <tusharbansal@iitbhilai.ac.in>
2023-12-03 12:50:34 +0530

    integrated pdf rendering

--- final evaluation/frontend/src/App.js
+++ final evaluation/frontend/src/App.js
@@ -6,12 +6,11 @@ function App() {
   const [prediction, setPrediction] = useState(null);
   const [textInput, setTextInput] = useState('');
   const [imagePreview, setImagePreview] = useState(null);
-
+  const [pdfContent, setPdfContent] = useState(null);
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
     setFile(selectedFile);
 
-  
     const previewURL = URL.createObjectURL(selectedFile);
     setImagePreview(previewURL);
   };
@@ -20,10 +19,13 @@ function App() {
     if (file) {
       const formData = new FormData();
       formData.append('file', file);
-
+  
       axios.post('http://127.0.0.1:5000/predict', formData)
         .then(response => {
+          console.log(response.data);
           setPrediction(response.data.prediction);
+  
+          setPdfContent(response.data.pdfcontent);
         })
         .catch(error => console.error('Error:', error));
     } else if (textInput.trim() !== '') {
@@ -39,21 +41,21 @@ function App() {
         <h1 className="text-2xl font-bold">RayPort</h1>
       </nav>
 
-      <div className='flex mx-8 my-4 justify-center'>
-        <div className='w-1/4'>
-        {imagePreview && (
-          <div className="flex-shrink-0 mx-auto">
-            <img
-              src={imagePreview}
-              alt="Preview"
-              className="mb-4 w-60 h-60 rounded-md shadow"
-              style={{ maxWidth: '100%', maxHeight: '600px' }}  // Increase the max height
-            />
-          </div>
-        )}
+      <div className="flex mx-8 my-4 justify-center">
+        <div className="w-1/4">
+          {imagePreview && (
+            <div className="flex-shrink-0 mx-auto">
+              <img
+                src={imagePreview}
+                alt="Preview"
+                className="mb-4 w-60 h-60 rounded-md shadow"
+                style={{ maxWidth: '100%', maxHeight: '600px' }}
+              />
+            </div>
+          )}
         </div>
-        <div className='w-3/4'>
-        {prediction && (
+        <div className="w-1/4">
+          {prediction && (
             <div className="mb-8">
               <h2 className="text-xl font-bold mb-2">Prediction:</h2>
               <div className="">
@@ -77,8 +79,20 @@ function App() {
             </div>
           )}
         </div>
-      </div>
+        <div className="w-2/4">
+        {pdfContent && (    
+        <div className="">
 
+          <iframe
+            title="Generated PDF"
+            src={`data:application/pdf;base64,${pdfContent}`}
+            height="700px"
+            width="100%"
+          ></iframe>
+        </div>
+      )}
+        </div>
+      </div>
 
       <div className="fixed bottom-0 left-0 right-0 p-4 bg-white flex">
         {/* Image Upload Section */}
